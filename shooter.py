a7acfb83a1ea5f214d2632831c3e775e87686105
Eotteo98 <cosma.teo@yahoo.com>
2025-04-04 18:49:06 +0200

    Add files via upload

--- shooter.py
+++ shooter.py
@@ -21,7 +21,7 @@ shooter.dead = False
 shooter.countdown = 90
 
 def display_score():
-    screen.draw.text("Your score is   " + str(score),(700, 50))
+    screen.draw.text("Your score is   " + str(score),(70, 50))
 
 def game_over():
     screen.draw.text("Game Over", (400, 300))
@@ -31,7 +31,7 @@ def on_key_down(key):
         if key == keys.SPACE:
             bullets.append(Actor("bullet.png"))
             bullets[-1].x = shooter.x
-            bullets [-1].y = shooter.y = -50
+            bullets [-1].y = shooter.y -50
         
 
 def update():
@@ -56,5 +56,47 @@ def update():
     
     if len(bugs) == 0:
         game_over()
+    
+    if len(bugs) >0 and (bugs[-1].x > WIDTH - 60 or bugs [0].x < 60):
+        move_down = True
+        direction = direction * -1
+    
+    for bug in bugs:
+        bug.x += 5 * direction
+        if move_down == True:
+            bug.y += 80
 
+        if bug.y > HEIGHT:
+            bugs.remove(bug)
         
+        for bullet in bullets:
+            if bug.colliderect(bullet):
+                score += 100
+                bullets.remove(bullet)
+                bugs.remove(bug)
+                if len(bugs) == 0:
+                    game_over()
+        
+        if bug.colliderect(shooter):
+            shooter.dead = True
+    if shooter.dead:
+        shooter.countdown -= 1
+    if shooter.countdown == 0:
+        shooter.dead = False
+        shooter.countdown = 60
+
+def draw():
+    screen.clear()
+    screen.blit("space", (0,0))
+    for bullet in bullets:
+        bullet.draw()
+    for bug in bugs:
+        bug.draw()
+    if shooter.dead == False:
+        shooter.draw()
+    display_score()
+
+    if len(bugs) == 0:
+        game_over()
+
+pgzrun.go()
